81e44a49d909ce6e5a472404007c27120057a43b
adinai-temirbek <adinai131004@gmail.com>
2025-11-22 23:14:33 +0600

    changed main

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -9,15 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App />
     </React.StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 6f0579372b85e7d6af9de3e0ab0c332903ba8544
